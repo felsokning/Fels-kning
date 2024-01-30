91ab635cf2f7093d429742af55d557de2d74bef1
felsokning <github@felsokning.se>
2024-01-30 18:08:59 +0000

    Adding More Extensions for Collections to Chain Queries

--- Felsökning.Tests/CollectionExtensionsTests.cs
+++ Felsökning.Tests/CollectionExtensionsTests.cs
@@ -52,5 +52,155 @@ namespace Felsökning.Tests
                 item.Should().BeGreaterThan(0);
             }
         }
+
+        [TestMethod]
+        public void CollectionOfType_OrderByChained_ShouldReturnIOrderedEnumerable()
+        {
+            var list = new Collection<SampleJson>
+            {
+                new() { Id = 1, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "B Test Title", UserId = 1 },
+            };
+
+            var sut = (ICollection<SampleJson>)list;
+            var result = sut.OrderByChained([x => x.Id, x => x.UserId]);
+            result.Should().NotBeEmpty();
+            var resultingList = result.ToList();
+            var firstRow = resultingList[0];
+            firstRow.Id.Should().Be(1);
+            firstRow.UserId.Should().Be(0);
+            var secondRow = resultingList[1];
+            secondRow.Id.Should().Be(2);
+            secondRow.UserId.Should().Be(0);
+            var thirdRow = resultingList[2];
+            thirdRow.Id.Should().Be(2);
+            thirdRow.UserId.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void EnumerableOfType_OrderByChained_ShouldReturnIOrderedEnumerable()
+        {
+            var list = new List<SampleJson>
+            {
+                new() { Id = 1, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "B Test Title", UserId = 1 },
+            };
+
+            var sut = (IEnumerable<SampleJson>)list;
+
+            var result = sut.OrderByChained([x => x.Id, x => x.UserId]);
+            result.Should().NotBeEmpty();
+            var resultingList = result.ToList();
+            var firstRow = resultingList[0];
+            firstRow.Id.Should().Be(1);
+            firstRow.UserId.Should().Be(0);
+            var secondRow = resultingList[1];
+            secondRow.Id.Should().Be(2);
+            secondRow.UserId.Should().Be(0);
+            var thirdRow = resultingList[2];
+            thirdRow.Id.Should().Be(2);
+            thirdRow.UserId.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void ListOfType_OrderByChained_ShouldReturnIOrderedEnumerable()
+        {
+            var list = new List<SampleJson>
+            {
+                new() { Id = 1, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "B Test Title", UserId = 1 },
+            };
+
+            var result = list.OrderByChained([x => x.Id, x => x.UserId]);
+            result.Should().NotBeEmpty();
+            var resultingList = result.ToList();
+            var firstRow = resultingList[0];
+            firstRow.Id.Should().Be(1);
+            firstRow.UserId.Should().Be(0);
+            var secondRow = resultingList[1];
+            secondRow.Id.Should().Be(2);
+            secondRow.UserId.Should().Be(0);
+            var thirdRow = resultingList[2];
+            thirdRow.Id.Should().Be(2);
+            thirdRow.UserId.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void CollectionOfType_OrderByDescendingChained_ShouldReturnIOrderedEnumerable()
+        {
+            var list = new Collection<SampleJson>
+            {
+                new() { Id = 1, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "B Test Title", UserId = 1 },
+            };
+
+            var sut = (ICollection<SampleJson>)list;
+            var result = sut.OrderByDescendingChained([x => x.Id, x => x.UserId]);
+            result.Should().NotBeEmpty();
+            var resultingList = result.ToList();
+            var firstRow = resultingList[0];
+            firstRow.Id.Should().Be(2);
+            firstRow.UserId.Should().Be(1);
+            var secondRow = resultingList[1];
+            secondRow.Id.Should().Be(2);
+            secondRow.UserId.Should().Be(0);
+            var thirdRow = resultingList[2];
+            thirdRow.Id.Should().Be(1);
+            thirdRow.UserId.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void EnumerableOfType_OrderByDescendingChained_ShouldReturnIOrderedEnumerable()
+        {
+            var list = new List<SampleJson>
+            {
+                new() { Id = 1, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "B Test Title", UserId = 1 },
+            };
+
+            var sut = (IEnumerable<SampleJson>)list;
+
+            var result = sut.OrderByDescendingChained([x => x.Id, x => x.UserId]);
+            result.Should().NotBeEmpty();
+            var resultingList = result.ToList();
+            var firstRow = resultingList[0];
+            firstRow.Id.Should().Be(2);
+            firstRow.UserId.Should().Be(1);
+            var secondRow = resultingList[1];
+            secondRow.Id.Should().Be(2);
+            secondRow.UserId.Should().Be(0);
+            var thirdRow = resultingList[2];
+            thirdRow.Id.Should().Be(1);
+            thirdRow.UserId.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void ListOfType_OrderByDescendingChained_ShouldReturnIOrderedEnumerable()
+        {
+            var list = new List<SampleJson>
+            {
+                new() { Id = 1, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "A Test Title", UserId = 0 },
+                new() { Id = 2, Completed = true, Title = "B Test Title", UserId = 1 },
+            };
+
+            var result = list.OrderByDescendingChained([x => x.Id, x => x.UserId]);
+            result.Should().NotBeEmpty();
+            var resultingList = result.ToList();
+            var firstRow = resultingList[0];
+            firstRow.Id.Should().Be(2);
+            firstRow.UserId.Should().Be(1);
+            var secondRow = resultingList[1];
+            secondRow.Id.Should().Be(2);
+            secondRow.UserId.Should().Be(0);
+            var thirdRow = resultingList[2];
+            thirdRow.Id.Should().Be(1);
+            thirdRow.UserId.Should().Be(0);
+        }
     }
 }
