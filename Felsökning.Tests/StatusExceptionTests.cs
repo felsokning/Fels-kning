c3cd81f7351cee7b91ccabe66655c9bbb31b7b8b
felsokning <46375514+felsokning@users.noreply.github.com>
2025-02-12 01:41:32 +0000

    Adding Test Cases to Increase Coverage

--- Felsökning.Tests/StatusExceptionTests.cs
+++ Felsökning.Tests/StatusExceptionTests.cs
@@ -10,60 +10,72 @@ namespace Felsökning.Tests
     [TestClass]
     public class StatusExceptionTests
     {
+        private static void AssertStatusException(StatusException sut, string expectedMessage, string? expectedStatusCode = null, Exception? expectedInnerException = null, HttpRecord? expectedHttpRecord = null)
+        {
+            sut.Should().BeOfType<StatusException>();
+            sut.Message.Should().NotBeNullOrWhiteSpace();
+            sut.Message.Should().Be(expectedMessage);
+
+            if (expectedStatusCode != null)
+            {
+                sut.StatusCode.Should().NotBeNullOrWhiteSpace();
+                sut.StatusCode.Should().Be(expectedStatusCode);
+            }
+
+            if (expectedInnerException != null)
+            {
+                sut.InnerException.Should().BeOfType<Exception>();
+                sut.InnerException?.Message.Should().Be(expectedInnerException.Message);
+            }
+
+            if (expectedHttpRecord != null)
+            {
+                sut.HttpRecord.Should().BeOfType<HttpRecord>();
+                sut.HttpRecord.Content.Should().NotBeNullOrWhiteSpace();
+                sut.HttpRecord.Content.Should().Be(expectedHttpRecord.Content);
+                sut.HttpRecord.Url.Should().NotBeNullOrWhiteSpace();
+                sut.HttpRecord.Url.Should().Be(expectedHttpRecord.Url);
+                sut.HttpRecord.StatusCode.Should().NotBeNullOrWhiteSpace();
+                sut.HttpRecord.StatusCode.Should().Be(expectedHttpRecord.StatusCode);
+                sut.HttpRecord.Method.Should().NotBeNullOrWhiteSpace();
+                sut.HttpRecord.Method.Should().Be(expectedHttpRecord.Method);
+                sut.HttpRecord.RequestId.Should().NotBeNullOrWhiteSpace();
+            }
+        }
+
         [TestMethod]
         public void StatusException_ctor()
         {
             var sut = new StatusException();
-
-            sut.Should().BeOfType<StatusException>();
+            AssertStatusException(sut, "Exception of type 'Felsökning.StatusException' was thrown.");
         }
 
         [TestMethod]
         public void StatusException_Message()
         {
             var sut = new StatusException("On Fire");
-
-            sut.Should().BeOfType<StatusException>();
-            var message = sut.Message;
-            message.Should().NotBeNullOrWhiteSpace();
-            message.Should().Be("Invalid status given in response: On Fire");
+            AssertStatusException(sut, "Invalid status given in response: On Fire");
         }
 
         [TestMethod]
         public void StatusException_StatusCode_Message()
         {
             var sut = new StatusException("404", "On Fire");
-
-            sut.Should().BeOfType<StatusException>();
-            var message = sut.Message;
-            message.Should().NotBeNullOrWhiteSpace();
-            message.Should().Be("Invalid status response received. Status: 404. Message: On Fire");
-            var status = sut.StatusCode;
-            status.Should().NotBeNullOrWhiteSpace();
-            status.Should().Be("404");
+            AssertStatusException(sut, "Invalid status response received. Status: 404. Message: On Fire", "404");
         }
 
         [TestMethod]
         public void StatusException_WrappedException()
         {
             var baseException = new Exception("The Science is leaking out");
-
             var sut = new StatusException("On Fire", baseException);
-
-            sut.Should().BeOfType<StatusException>();
-            var message = sut.Message;
-            message.Should().NotBeNullOrWhiteSpace();
-            message.Should().Be("Invalid status given in response: On Fire");
-            var innerException = sut.InnerException;
-            innerException.Should().BeOfType<Exception>();
-            innerException?.Message.Should().Be("The Science is leaking out");
+            AssertStatusException(sut, "Invalid status given in response: On Fire", expectedInnerException: baseException);
         }
 
         [TestMethod]
         public void StatusException_WrappedException_HttpRecord()
         {
             var baseException = new Exception("The Science is leaking out");
-
             var httpRecord = new HttpRecord
             {
                 Content = "Stuff is on fire",
@@ -72,32 +84,8 @@ namespace Felsökning.Tests
                 Method = "GET",
                 RequestId = Guid.NewGuid().ToString(),
             };
-
             var sut = new StatusException("On Fire", baseException, httpRecord);
-
-            sut.Should().BeOfType<StatusException>();
-            var record = sut.HttpRecord;
-            record.Should().BeOfType<HttpRecord>();
-            var content = record.Content;
-            content.Should().NotBeNullOrWhiteSpace();
-            content.Should().Be("Stuff is on fire");
-            var url = record.Url;
-            url.Should().NotBeNullOrWhiteSpace();
-            url.Should().Be("https://www.somefakedomain.co.gov.de.com");
-            var statusCode = record.StatusCode;
-            statusCode.Should().NotBeNullOrWhiteSpace();
-            statusCode.Should().Be("404");
-            var method = record.Method;
-            method.Should().NotBeNullOrWhiteSpace();
-            method.Should().Be("GET");
-            var requestId = record.RequestId;
-            requestId.Should().NotBeNullOrWhiteSpace();
-            var message = sut.Message;
-            message.Should().NotBeNullOrWhiteSpace();
-            message.Should().Be("Invalid status given in response: On Fire");
-            var innerException = sut.InnerException;
-            innerException.Should().BeOfType<Exception>();
-            innerException?.Message.Should().Be("The Science is leaking out");
+            AssertStatusException(sut, "Invalid status given in response: On Fire", expectedInnerException: baseException, expectedHttpRecord: httpRecord);
         }
 
         [TestMethod]
@@ -111,32 +99,8 @@ namespace Felsökning.Tests
                 Method = "GET",
                 RequestId = Guid.NewGuid().ToString(),
             };
-
             var sut = new StatusException("404", "On Fire", httpRecord);
-
-            sut.Should().BeOfType<StatusException>();
-            var record = sut.HttpRecord;
-            record.Should().BeOfType<HttpRecord>();
-            var content = record.Content;
-            content.Should().NotBeNullOrWhiteSpace();
-            content.Should().Be("Stuff is on fire");
-            var url = record.Url;
-            url.Should().NotBeNullOrWhiteSpace();
-            url.Should().Be("https://www.somefakedomain.co.gov.de.com");
-            var statusCode = record.StatusCode;
-            statusCode.Should().NotBeNullOrWhiteSpace();
-            statusCode.Should().Be("404");
-            var method = record.Method;
-            method.Should().NotBeNullOrWhiteSpace();
-            method.Should().Be("GET");
-            var requestId = record.RequestId;
-            requestId.Should().NotBeNullOrWhiteSpace();
-            var message = sut.Message;
-            message.Should().NotBeNullOrWhiteSpace();
-            message.Should().Be("Invalid status response received. Status: 404. Message: On Fire");
-            var status = sut.StatusCode;
-            status.Should().NotBeNullOrWhiteSpace();
-            status.Should().Be("404");
+            AssertStatusException(sut, "Invalid status response received. Status: 404. Message: On Fire", "404", expectedHttpRecord: httpRecord);
         }
     }
 }
